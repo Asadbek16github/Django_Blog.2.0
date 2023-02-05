84ce6b913a635406185ac1e3458972307879c4cc
Asadbek16 <asadbekshomurodov96@gmail.com>
2023-02-05 21:40:45 +0500

    Yangi userlar modeli yaratildi

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CostomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=False, blank=False)
+    birthYear = models.PositiveIntegerField(null=False, blank=False)
+    living_country = models.CharField(max_length=100, null=False, blank=False)
+    living_city = models.CharField(max_length=100, null=False, blank=False)
+
