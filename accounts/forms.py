7c6af728715847ef6a01531a1bb683af05782d3d
Asadbek16 <asadbekshomurodov96@gmail.com>
2023-02-05 21:57:12 +0500

    Malumotlar ombori sqlite qo'shildi, foydalanuvchilar malumotlari uchun model yaratildi, va yaratilgan model uchun formalar yozildi

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CostomUsers 
+
+
+class CostomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CostomUsers
+        fields = ('username', 'first_name', 'last_name', 'email', 'age', 'birthYear', 'living_contry', 'living_city')
+
+
+class CostomUsersChangeForm(UserChangeForm):
+    class Meta:
+        model = CostomUsers
+        fields = ('first_name', 'last_name', 'email', 'age', 'birthYear', 'living_contry', 'living_city')
--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class CostomUser(AbstractUser):
+class CostomUsers(AbstractUser):
     age = models.PositiveIntegerField(null=False, blank=False)
     birthYear = models.PositiveIntegerField(null=False, blank=False)
     living_country = models.CharField(max_length=100, null=False, blank=False)
