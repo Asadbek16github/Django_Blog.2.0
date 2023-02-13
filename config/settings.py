de166a1f207bb17b388b6993baa16935951a54e6
Asadbek16 <asadbekshomurodov96@gmail.com>
2023-02-13 17:05:13 +0500

    Foydalanuvchi uzini akkaunti ichida o'zini parolini bilgan holda parolini almashtirish imkoniyati qo'shildi

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CostomUsers'
 
