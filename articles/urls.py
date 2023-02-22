4c5464c984c69359222d906d602385fce0885dad
Asadbek16 <asadbekshomurodov96@gmail.com>
2023-02-22 21:57:42 +0500

    maqolalarni tahrirlash, o'chirish, yaratish imkoniyatlari yaratildi

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import ArticlesView, ArticleDetailView
+from .views import ( 
+                        ArticlesView, 
+                        ArticleDetailView, 
+                        ArticleDeleteView, 
+                        ArticleUpdateView,
+                        ArticleCreateView
+                    )
 
 urlpatterns = [
     path('<int:pk>/', ArticleDetailView.as_view(), name='articleDetail'),
     path('', ArticlesView.as_view(), name='articlesPage'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='deleteObject'),
+    path('<int:pk>/update/', ArticleUpdateView.as_view(), name='updateObject'),
+    path('create/', ArticleCreateView.as_view(), name='createObject'), 
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Articles
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 class ArticlesView(ListView):
@@ -11,3 +13,24 @@ class ArticlesView(ListView):
 class ArticleDetailView(DetailView):
     model = Articles
     template_name = 'detail.html'
+
+
+class ArticleDeleteView(DeleteView):
+    model = Articles
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('articlesPage')
+
+class ArticleUpdateView(UpdateView):
+    model = Articles
+    template_name = 'article_update.html'
+    fields = ('Sarlovha', 'Qisqacha_Matn', 'Matn', 'Rasm')
+    success_url = reverse_lazy('articlesPage')
+
+
+class ArticleCreateView(CreateView):
+    model = Articles
+    template_name = 'article_create.html'
+    fields = ('Sarlovha', 'Qisqacha_Matn', 'Matn', 'Rasm', 'Mualliff')
+    success_url = reverse_lazy('articlesPage')
+
+
