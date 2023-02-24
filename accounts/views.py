1c7c941c3bac7589dafc914f526805997d518151
Asadbek16 <asadbekshomurodov96@gmail.com>
2023-02-24 10:05:19 +0500

    Userlarga login qilmasdan saytning boshqa sahifalariga kirish taqiqlandi, Post qo'shganda avtomatik Mualliff nomini oladigan qilindi, maqolalarni faqat yaratgan odam o'chira oladi va yangilay oladigan qilindi, Va saytga faqat admin maqola qo'shadigan qilindi

--- accounts/views.py
+++ accounts/views.py
@@ -8,4 +8,4 @@ from django.urls import reverse_lazy
 class Signup_View(CreateView):
     form_class = CostomUserCreationForm
     template_name = 'registration/signup.html'
-    success_url = reverse_lazy
+    success_url = reverse_lazy('login')
--- articles/views.py
+++ articles/views.py
@@ -3,34 +3,51 @@ from django.views.generic import ListView, DetailView
 from .models import Articles
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
-class ArticlesView(ListView):
+class ArticlesView(LoginRequiredMixin, ListView):
     model = Articles
     template_name = 'article.html'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Articles
     template_name = 'detail.html'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Articles
     template_name = 'article_delete.html'
     success_url = reverse_lazy('articlesPage')
 
-class ArticleUpdateView(UpdateView):
+    def test_func(self):
+        ob = self.get_object()
+        return ob.Mualliff == self.request.user
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Articles
     template_name = 'article_update.html'
     fields = ('Sarlovha', 'Qisqacha_Matn', 'Matn', 'Rasm')
     success_url = reverse_lazy('articlesPage')
 
+    def test_func(self):
+        ob = self.get_object()
+        return ob.Mualliff == self.request.user
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, UserPassesTestMixin,  CreateView):
     model = Articles
     template_name = 'article_create.html'
-    fields = ('Sarlovha', 'Qisqacha_Matn', 'Matn', 'Rasm', 'Mualliff')
+    fields = ('Sarlovha', 'Qisqacha_Matn', 'Matn','Rasm')
     success_url = reverse_lazy('articlesPage')
+
+    def form_valid(self, form):
+        form.instance.Mualliff = self.request.user
+        return super().form_valid(form)
+    
+
+    def test_func(self):
+        return self.request.user.is_superuser
 
 
